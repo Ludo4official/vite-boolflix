d1f413beb325020541095d6713f04406c30ef590
Ludovico Longhini <ludovico.longhini@gmail.com>
2023-02-09 13:21:54 +0100

    Boolflix

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import { store } from './components/store.js';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  name:'App',
+  data() {
+    return {
+      store
+    }
+  },
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
+</script>
+
+<template>
+<AppHeader></AppHeader>
+<AppMain></AppMain>
+<AppFooter></AppFooter>
+</template>
+
+<style lang="scss">
+@import './components/styles/partials/main.scss';
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name:'AppHeader',
+    // data() {
+    //     return
+    // }
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1 class="text-center">Ciao</h1>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
+node_modules/bootstrap/scss/bootstrap
